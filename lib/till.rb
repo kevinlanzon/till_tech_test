6921e81a67d4c86270799d717576b8ef297af96d
Kevin Lanzon <kevinlanzon@gmail.com>
2015-05-17 14:05:40 +0100

    till starts with no orders

--- lib/till.rb
+++ lib/till.rb
@@ -1,9 +1,10 @@
 class Till
 
-  attr_reader :total, :tax_total
+  attr_reader :total, :tax_total, :orders
 
   def initialize
     @total = 0
     @tax_total = 0
+    @orders = {}
   end
 end
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -9,4 +9,8 @@ describe Till do
   it 'starts with a tax value of 0' do
     expect(subject.tax_total).to eq 0
   end
+
+  it 'starts with no orders' do
+    expect(subject.orders.empty?).to be true
+  end
 end
